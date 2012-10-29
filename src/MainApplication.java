630bc120b1f3976d7604bce4e66ae9620c64db0b
Jack <jackp@vayoungs.us>
2012-10-28 20:17:05 -0400

    Removed editprofileview methods from the main application file

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -336,7 +336,7 @@ public class MainApplication {
 		
 		if (c == 1) {
 			//go to edit profile
-			editProfileView(usersIndex);
+			EditProfileView.editProfileView(usersIndex);
 		} else if (c == 2) {
 			//go to view teams profiles
 			//viewAllTeams(usersIndex);
@@ -389,7 +389,7 @@ public class MainApplication {
 		if (c == 1) {
 			//go to edit profile
 			System.out.println(timeStamp() + "- SECTION: Dashboards.   Method Called: coachesDashBoardView()   ACTION: call editProfileView() method");
-			editProfileView(usersIndex);
+			EditProfileView.editProfileView(usersIndex);
 		} else if (c == 2) {
 			//go to view teams profiles
 			System.out.println(timeStamp() + "- SECTION: Dashboards.   Method Called: coachesDashBoardView()   ACTION: call viewTeamProfiles() method");
@@ -421,7 +421,7 @@ public class MainApplication {
 		
 		if (c == 1) {
 			//go to edit profile
-			editProfileView(usersIndex);
+			EditProfileView.editProfileView(usersIndex);
 		} else if (c == 2) {
 			//go to view teams profiles
 			viewTeamProfiles(usersIndex);
@@ -558,34 +558,7 @@ public class MainApplication {
 /***************************************************************************************************/	
 /********************************Start: Edit View ***********************************/
 	
-	public static void editProfileView(int usersIndex) {
-		//"1. Password\n2. Phone Number\n3. Name"
-		String message = "What would you like to edit?\n1. Password\n2. Phone Number\n3. Name\n4. Go back to Dashboard";
-		String titleString = "Edit Profile - " + getController().getUc().getUserName(usersIndex);
-		
-		String r = InputHelper.promptStringMenuOptions(message, "Edit Profile", titleString, new int[]{1, 2, 3, 4});
-		
-		int editMenuSelection = Integer.parseInt(r);
-		
-		if (editMenuSelection == 1) {
-			//edit password
-			//-first prompt for them to entered their existing password
-			//if correct allow them to enter in a new password
-			//then display a successful message dialog
-			
-			editMenuPasswordOption(usersIndex);
-		} else if (editMenuSelection == 2) {
-			//edit phone number
-			editMenuPhoneNumber(usersIndex);
-		} else if (editMenuSelection == 3) {
-			//edit name
-			editMenuName(usersIndex);
-		} else if (editMenuSelection == 4) {
-			//Just added, not specified in directions. Go back to dashboard
-			playersDashBoardView(usersIndex);
-		}
-		
-	}
+
 	
 	/*****/
 	//delete this portion 
@@ -593,118 +566,8 @@ public class MainApplication {
 		System.out.println("----------player -----------");
 		System.out.println(p);
 	}
-	
-	/****/
-	
-	@SuppressWarnings("unused")
-	public static void editMenuName(int usersIndex) {
-		
-		String curNameConcat = getController().getUc().getUserObject(usersIndex).getFirstName() + " " + getController().getUc().getUserObject(usersIndex).getLastName();
-		String data = "The current name is: " + curNameConcat;
-			data += "\nPlease enter in the new name, first and last name seperated by a space: ";
-		
-		String newName = InputHelper.promptString(data, "newName", "Edit - name");
-		System.out.println("Entered name is now : " + newName + " where as before it was " + curNameConcat);
-		
-		//Need to check and make sure they actually entered a space between the first and last name to delimite and set attribtures
-		if (newName == null ) {
-			//they pressed the cancel button
-			System.out.println("Pressed the cancel button on editMenuName");
-		} else if (newName != null) {
-			//they entered in something for their name, now we just need to valdiate it for a delimite to set the first and last name attribtues
-			String delims = "[ ]+";
-			String[] tokens = newName.split(delims);
 
-			if (tokens.length == 2) {
-				//the entered name had a space, so the name was set
-				getController().getUc().getUserObject(usersIndex).setFirstName(tokens[0]);
-				getController().getUc().getUserObject(usersIndex).setLastName(tokens[1]);
-				InputHelper.successfulEditDialog("name", "Edit - name");
-				editProfileView(usersIndex);
-			} else {
-				//error they did not enter in a space between the name they enter so 
-				//as a result you cant set the first and last name
-				InputHelper.unsuccessfulEditDialog("name", "Edit - name");
-				editProfileView(usersIndex);
-			}
-		
-		} else {
-			System.out.println("They pressed the exit button on the editMenuName method");
-		}	
-	}
-	
-	@SuppressWarnings("unused")
-	public static void editMenuPhoneNumber(int usersIndex) {
-		String data = "Your current phone number is : " + getController().getUc().getUserObject(usersIndex).getPhone();
-			data += "\nPlease enter the new phone number (xxx xxx xxxx):";
-		
-		//need to add validatation to this phone input area.  ex. 703 252 0854
-		String newNumber = InputHelper.promptString(data, "phoneNumber", "Edit - phone number");
-		
-		System.out.println("The new phone number they entered: " + newNumber);
-		
-		if (newNumber == null) {
-			//they pressed the cancel button
-			System.out.println("Pressed the cancel button on editMenuPhoneNumber");
-			
-		} else if (newNumber != null) {
-			// place phone number validation here
-			System.out.println("entered in something for the new phone number");
-			
-			int l = newNumber.length();
-			System.out.println("Phone number length is : " + l);
-			if (l == 12) {
-				getController().getUc().getUserObject(usersIndex).setPhone(newNumber);
-				InputHelper.successfulEditDialog("phone number", "Edit - phone number");
-			} else {
-				InputHelper.unsuccessfulEditDialog("phone number", "Edit - phone number");
-			}
-		} else {
-			System.out.println("They pressed the exit button on the editMenuPHoneNumber method");
-		}
-		
-		editProfileView(usersIndex);
-	}
-	
-	
-	@SuppressWarnings("unused")
-	public static void editMenuPasswordOption(int usersIndex){
-		String previousPass = InputHelper.promptString("Please enter your old password:", "old password ", "Edit - password");
-		
-		if (previousPass == null) {
-			//they pressed the cancel button on the prompt for entering previous password
-			editProfileView(usersIndex);
-		} else if (previousPass != null) {
-			//inserted a none null password
-			
-			//validate their existing password for match
-			if (previousPass.equals(getController().getUc().getPassword(usersIndex))){
-				String newPassword = InputHelper.promptString("Please enter your new password:", "new password", "Edit - password");
-				
-				if (newPassword == null) {
-					//they pressed the cancel button on the prompt for entering their new password
-					//they will be sent back to edit profile view
-					editProfileView(usersIndex);
-				} else if (newPassword != null) {
-					//they entered something for the new password
-					
-					System.out.println("You changed exist password: " + getController().getUc().getPassword(usersIndex) + " to : " + newPassword);
-					//getUc().getPassword(usersIndex) = newPassword;
-					getController().getUc().setPassword(usersIndex, newPassword);
-					System.out.println("After added new method changed exist password: " + getController().getUc().getPassword(usersIndex) + " to : " + newPassword);
-					InputHelper.successfulEditDialog("password", "Successful edit");
-					editProfileView(usersIndex);
-				}
-			} else {
-				JOptionPane.showMessageDialog(null, "Sorry! The exiting password you entered does not match our records");
-				//redirects them back to the editProfileView
-				editProfileView(usersIndex);
-			}
-		} else {
-			System.out.println("They pressed the exit button on the edtiMenuPasswordOption");
-			editProfileView(usersIndex);
-		}
-	}	
+
 	
 
 	//Other methods not sure where to place yet
@@ -746,18 +609,5 @@ public class MainApplication {
 	    return a;
 	}
 
-	
-	//temp method
-//	public static void viewAllPlayersListing(int usersIndex){
-//		String a = "";
-//		for (int i = 0; i < getController().getUc().getUserObjectArraylist().size(); i++) {
-//			a += "#" + (i+1) + "  " + OutputHelpers.giveConcatName(getController().getUc().getUserObject(i));
-//			a += "\n";
-//		}
-//		
-//		InputHelper.displayMessage(a, "View all people");
-//		officialsDashBoardView(usersIndex);
-//	}
-	
 	
 }
