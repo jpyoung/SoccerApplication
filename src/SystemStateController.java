a4f7c42acc63f6b9e1bba48a75b193d11b24aef2
Jack <jackp@vayoungs.us>
2012-11-03 13:30:32 -0400

    clean up systemstatecontroller

--- src/SystemStateController.java
+++ src/SystemStateController.java
@@ -11,26 +11,31 @@ import Models.Coach;
 import Models.User;
 import controller.Controller;
 
+/**
+ * @author Jack Young
+ * @date Nov 3, 2012
+ */
 public class SystemStateController {
 
+	public static void loadEverything() {
+		MainApplication.controller = readIn();
+		//MainApplication.controller = readInDefault();
+		MainApplication.getController().getUc().outputSystemUsersTable();
+		findCoachesAndAddThereTeams();
+	}
+	
 	public static void findCoachesAndAddThereTeams() {
 		ArrayList<User> u = MainApplication.getController().getUc().getUserObjectArraylist();
 		ArrayList<Coach> coaches = new ArrayList<Coach>();
 		for (int i = 0; i < u.size(); i++) {
-			// System.out.println(u.get(i).getClass().getName());
 			if (u.get(i).getClass().getName().equals("Models.Coach")) {
 				coaches.add((Coach) u.get(i));
 			}
 		}
-		// System.out.println("Coaches Array Size: " + coaches.size());
-		// System.out.println("Controller Team Size: " +
-		// MainApplication.getController().getTeam().size());
 
 		// clear existing teams, then will refill the team arraylist var
 		MainApplication.getController().getTeam().clear();
-
-		
-
+		//now re adding the teams from data storage
 		for (int z = 0; z < coaches.size(); z++) {
 			if (coaches.get(z).getHasTeam()) {
 				MainApplication.getController().addATeam(coaches.get(z).getTeam());
@@ -39,22 +44,12 @@ public class SystemStateController {
 
 	}
 
-	public static void loadEverything() {
-		MainApplication.controller = readIn();
-		//MainApplication.controller = readInDefault();
-		MainApplication.getController().getUc().outputSystemUsersTable();
-		findCoachesAndAddThereTeams();
-	}
-
 	public static Controller readIn() {
 		try {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-					"TeamssArr.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("TeamssArr.dat"));
 			Controller uu = (Controller) in.readObject();
 			in.close();
-			System.out
-					.println(OutputHelpers.timeStamp()
-							+ "----------------READ IN From DATA File-------------------");
+			System.out.println(OutputHelpers.timeStamp() + "----------------READ IN From DATA File---------------");
 			return uu;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -66,13 +61,10 @@ public class SystemStateController {
 	
 	public static Controller readInDefault() {
 		try {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-					"defaultBaseState.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("defaultBaseState.dat"));
 			Controller uu = (Controller) in.readObject();
 			in.close();
-			System.out
-					.println(OutputHelpers.timeStamp()
-							+ "----------------READ IN DEFAULT DATA File-------------------");
+			System.out.println(OutputHelpers.timeStamp() + "----------------READ IN DEFAULT DATA File---------------");
 			return uu;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -93,10 +85,7 @@ public class SystemStateController {
 			System.out.println("------team size on close-" + MainApplication.getController().getTeam().size());
 			out.writeObject(MainApplication.getController());
 			out.close();
-
-			System.out.println(OutputHelpers.timeStamp()
-					+ "--------------SAVED STATE DEFAULT DATA FILE---------");
-
+			System.out.println(OutputHelpers.timeStamp() + "--------------SAVED STATE DEFAULT DATA FILE---------");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -105,16 +94,11 @@ public class SystemStateController {
 
 	public static void writeOut() {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(
-					new FileOutputStream("TeamssArr.dat"));
-			System.out.println("------team size on close-"
-					+ MainApplication.getController().getTeam().size());
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("TeamssArr.dat"));
+			System.out.println("------team size on close-" + MainApplication.getController().getTeam().size());
 			out.writeObject(MainApplication.getController());
 			out.close();
-
-			System.out.println(OutputHelpers.timeStamp()
-					+ "--------------SAVED STATE TO DATA FILE---------");
-
+			System.out.println(OutputHelpers.timeStamp() + "--------------SAVED STATE TO DATA FILE---------");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
